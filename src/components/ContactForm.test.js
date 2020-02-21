1890032003cdf2772a5fa69198f74e87d7a372e2
rdowd <rjarrettdowd@yahoo.com>
2020-02-20 22:31:15 -0500

    added a test to test if form submits

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import ContactForm from './ContactForm';
 import 'mutationobserver-shim';
+
 
 
 test("ContactForm renders without crashing.", () => {
@@ -9,10 +10,39 @@ test("ContactForm renders without crashing.", () => {
 });
 
 test("Check for form inputs", () => {
-    const { findByLabelText } = render(<ContactForm/>);
+    const { getByLabelText } = render(<ContactForm/>);
 
-    findByLabelText(/first name*/i);
-    findByLabelText(/last name*/i);
-    findByLabelText(/email*/i);
-    findByLabelText(/message/i);
+    getByLabelText(/first name*/i);
+    getByLabelText(/last name*/i);
+    getByLabelText(/email*/i);
+    getByLabelText(/message/i);
+    //had to add id to lables for getByLabelText to work
+});
+
+test("Check if form submits values", () => {
+   
+    const { getByLabelText, getByText, getByTestId } = render(<ContactForm/>);
+    const fname = getByLabelText(/first name*/i);
+    const lname = getByLabelText(/last name*/i);
+    const email =getByLabelText(/email*/i);
+    const message = getByLabelText(/message/i);
+
+    fireEvent.change(fname, { target : {value: "test fname" } });
+    fireEvent.change( lname, { target: { value: 'test lname' } });
+    fireEvent.change( email, { target: { value: 'test@someemail.com' } });
+    fireEvent.change( message, { target: { value: 'test message' } });
+
+    expect(fname.value).toBe("test fname");
+    expect(lname.value).toBe("test lname");
+    expect(email.value).toBe("test@someemail.com");
+    expect(message.value).toBe("test message");
+
+    
+    fireEvent.click(getByTestId(/submit/i));
+    
+    const dataText = getByTestId(/data/i);
+    
+    expect(dataText);
+    
+
 });
